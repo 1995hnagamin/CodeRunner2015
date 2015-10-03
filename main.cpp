3567e608c7671977338c89b344a789f179dcaa81
Hideaki Nagamine <hnagamin@kmc.gr.jp>
2015-10-03 23:03:15 +0900

    rescue from local maximum

--- main.cpp
+++ main.cpp
@@ -22,11 +22,26 @@ random_device rnd;
 int main() {
   vector<pair<int,Dat>> window, nextgen;
   Dat v;
-  window.emplace_back(0, v);
+  
+  // initialize window
+  int n; cin >> n;
+  REP(i, n) {
+    int size, score; 
+    cout << ">";
+    cin >> size >> score;
+    Dat dat;
+    REP(j, size) {
+      int x; cin >> x;
+      dat.push_back(x);
+    }
+    window.emplace_back(score, dat);
+  }
+  cout << "end initializtion" << endl;
+
   while (true) {
     for (auto cand__ : window) {
       auto cand_ = cand__.second;
-      REP(i_, 7) REP(j_, 7) {
+      REP(i_, 6) REP(j_, 6) {
         Dat cand = cand_;
         int i = 0, j = 0;
         while (i == j ||
@@ -55,6 +70,19 @@ int main() {
     }
     window = nextgen;
     nextgen.clear();
+    
+    // rescue from local max
+    int mim = 1000000, mxm = -1;
+    for (auto win: window) {
+      mim = min(mim, win.first);
+      mxm = max(mxm, win.first);
+    }
+    if (mxm - mim < 100) {
+      for (int i = 2; i < beam_width; ++i) {
+        window[i].second.erase(window[i].second.begin());
+        window[i].first = 0;
+      }
+    }
   }
   return 0;
 }
